b8297369ec7155f5061f6d5fb135df68a58fdf33
akarasenko <asya2794@mail.ru>
2020-06-08 22:41:49 +0700

    reading data from server added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# tensor-course
+To run application:
+in root folder run 'npm i' and then 'run server.js'
+
+To run data server:
+in folder dataServer run 'npm i' and then 'node server.js'
+Server is run on port 8080
--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-
+import {DataSet} from './dataSet.js';
 import {School} from './school.js';
 import {Header} from './header.js';
 import { Logo } from './logo.js';
@@ -11,66 +11,19 @@ logo.mount(headerHtml);
 let header = new Header();
 header.render();
 
-let infoHeader = document.querySelector('.info');
 header.mount(headerHtml);
-
-const peopleArr = [
-    {
-        type: 'student',
-        fullName: 'Маша',
-        university: 'УГАТУ',
-        course: 2,
-        birthDate: new Date(2000, 2, 1),
-        photoUrl: '/images/avatars/ava1.jpg'
-    },
-    {
-        type: 'student',
-        fullName: 'Саша',
-        university: 'МГУ',
-        course: 4,
-        birthDate: new Date(1997, 12, 31),
-        photoUrl: '/images/avatars/ava2.jpg'
-    },
-    {
-        type: 'student',
-        fullName: 'Анатолий',
-        university: 'МГУ',
-        course: 4,
-        birthDate: new Date(1994, 11, 31),
-        photoUrl: '/images/avatars/ava3.jpg'
-    },
-    {
-        type: 'teacher',
-        fullName: 'Андрей Иванович',
-        position: 'Frontend разрабтчик',
-        subject: 'Программирование на JavaScript',
-        birthDate: new Date(2000, 7, 19),
-        photoUrl: '/images/avatars/ava4.jpg'
-    },
-    {
-        type: 'teacher',
-        fullName: 'Василий Петрович',
-        position: 'Backend разрабтчик',
-        subject: 'Программирование на Java',
-        birthDate: new Date(2001, 4, 14),
-        photoUrl: '/images/avatars/ava5.jpg'
-    },
-    {
-        type: 'teacher',
-        fullName: 'Кузьма Александрович',
-        position: 'Тестировщик',
-        subject: 'Тестирование программного обеспечения',
-        birthDate: new Date(1999, 10, 11),
-        photoUrl: '/images/avatars/ava6.jpg'
-    }
-];
 
 const school = new School('Курс программирования');
 
-peopleArr.forEach((item) => {
-    school.add (item);
+let dataSet = new DataSet();
+
+let peopleArr = dataSet.readAll().then(result => {
+    result.forEach((item) => {
+        school.add (item);
+    })
 });
 
+console.log(school);
 
 
 
